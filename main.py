f10d4fa58f14c157bfdacfc654883c028e969c0a
siidom <99956085+siidom@users.noreply.github.com>
2023-03-26 13:26:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#importing things from arithmetic_arranger.py
+from arithmetic_arranger import arithmetic_arranger
+
+#unittest framework
+from unittest import main
+
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+
+main(module='test_module', exit=False)
